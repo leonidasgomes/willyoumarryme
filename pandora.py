59a6ea6d6011d61d1f9d80afda41cf65e52b10f7
Leonidas Gomes da Silva <lgdsilva@qualicorp.com.br>
2019-06-26 16:39:58 -0300

    Ajustei o Coração

--- pandora.py
+++ pandora.py
@@ -16,7 +16,7 @@ def casar(respo):
 
         frase = 'Eu te Amo'+nome
 
-        print('\n'.join([''.join([(frase[(x-y) % len(frase)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+        print('\n'.join([''.join([(frase[(x-y) % len(frase)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(15, -15, -1)]))
         
     elif respo == 'N':
 
