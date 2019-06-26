33323b63f7349e900d5e3cea1b011b815b547b58
Leonidas Gomes da Silva <lgdsilva@qualicorp.com.br>
2019-06-26 14:34:06 -0300

    Versão com coração de texto

--- pandora.py
+++ pandora.py
@@ -5,22 +5,30 @@ Desenvolvido por leonidasgomes
 Visite meu guithub:leonidasgomes
 Contribuindo com Amor :)
 '''
+
+nome=''
+
 #funcao que trata a resposta do casamento
-def casar(resp):
-resp=resp.upper()
-if resp == 'S':
+def casar(respo):
+    respagain=''
+    respo = respo.upper()
+    if respo == 'S':
 
-  print('S2')
+        frase = 'Eu te Amo'+nome
 
-elif resp == 'N':
+        print('\n'.join([''.join([(frase[(x-y) % len(frase)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+        
+    elif respo == 'N':
 
-  print('Bug')
+        print('Bug')
 
-else:
-  respagain = input('Resposta invalida responda S para sim e N Para nao:')   
+    else:
+        respagain = input('Resposta invalida responda S para sim e N Para nao:')   
   
-  casar(respagain)
+        casar(respagain)
   
+
+nome=input('Qual é o seu nome?')
 
 # Variavel com a Pergunta Inicial 
 pergunta='Voce quer casar comigo?'
